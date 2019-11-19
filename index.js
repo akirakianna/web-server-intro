ba69b8c22be4dc030410bfe25c0506fa3649da55
akirakianna <kianna.akiralove@gmail.com>
2019-11-19 21:08:14 +0000

    Object
    Address

--- index.js
+++ index.js
@@ -19,17 +19,55 @@ app.set('views', __dirname + '/public/views');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
+  var age = req.query.age;
   var name = req.query.name;
   if (!name) {
     name = "Unknown Person";
   }
-  res.render('hello', {name: name});
+
+  var templateData = {
+    name: name,
+    age: age,
+    address: address,
+  };
+
+  res.render('hello', templateData);
+
 });
 
 app.get('/test', function (req, res) {
   console.log(req.query);
-  res.send('OK');
+  res.send('Kianna');
+});
+
+app.get('/mydata', function (req, res){
+  var data = {
+    something: 1,
+    pet: "Mimi"
+  };
+  res.json(data);
+});
+
+app.get('/myname', function (req, res) {
+  console.log(req.query);
+  res.send('Kianna ' + 'Love');
+});
+
+app.get('/itsme', function (req, res) {
+ console.log(req.query);
+ var name = req.query.name;
+ if (name === 'Kianna') {
+   res.send("Hi " + name);
+  } else {
+    res.send("Unknown");
+  }
+
+});
+
+app.post('/mydata', function (req, res) {
+  console.log(req.body);
+  res.send('Posted');
 });
 
 app.post('/somedata', function (req, res) {
